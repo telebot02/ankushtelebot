757d1531374f022e46be0d95b9a6338eed75bb2c
telebot02 <telebot0102@gmail.com>
2024-08-31 10:52:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
-const { NewMessage } = require("telegram/events");
 const axios = require("axios");
 
-const testingChatId = "1493065360";
-const bonusChatId = "2247051543";
 const apiId = 28596369;
 const apiHash = "f50cfe3b10da015b2c2aa0ad31414a55";
-const sessionKey = "1BQANOTEuMTA4LjU2LjE2MgG7Lh+OYzvB3h6d58gAZWlsY9Vxr8G3NbqpkCXpsdrpSqJ3lXmDbK8aFiVVNNhBfsbceijMSKBDH3vUs04iwBVj03aJ1wA8i4kgWHWa1m0JeSoh6QBydndM3z5JLjGdXwspXfdM65L6KOsMrSE7IAiC5pltXmuZe6LtlAHzb9peQ7DTPd3YBLPumNZ2esqwGEL9OkfiwwiX48112Y2pBBO1jcXvlhElrqAK5lN9O/R2RBe1LhkU5Lxdt19zrtMzEd6IU//i0ko7DKEWHcMIqJWEp7kszx5pLKk+UERpzdQhRB8o1TV2JzvZ8rikDbh4kWZO5i69/Dyfj7h1tuK4EbPlfw==";
+const sessionKey = "1BQANOTEuMTA4LjU2LjE2MgG7eG+86rffrbVQla8pLlpfjwu1Jh9JKRpdGlYPcrlC+HJlt8QWnp/BTE/bl9sfMpGJ/nqjyrZsChWyRj8nf1kGxDsvmhNU0LCXhZaZiU4yM/EZ5EfBYASEo6F6m1FmixA6ZJPagbO2GScVhI7SLkZWr6RkBwzlTaL6+8SbbwYHcZF5+S+nGt8HFH+vSjNVx4hJh0E9cuuvS5H+lV7zwnQqV8kkDe6Pz62z+MfM0yAfBxfimmE7S/m+1N+R9DupN5WhUBGo4SjJwVE3K4eNC4SpaLnNimnVUhv5avGRL6Xg+lhl+DDzRs6wFt6PPzM4cBB/25d3Z/ucPGc7dvKT1546DQ==";
 const stringSession = new StringSession(sessionKey);
+
 const apiUrl = "https://colorwiz.cyou/mana/receive_red_packet";
 
+const session = new StringSession(sessionKey); 
+const client = new TelegramClient(session, apiId, apiHash, {});
+
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 const sendRedeemRequest = async (mobile, packetCode) => {
+    // console.log("inside send")
   try {
     const response = await axios.post(apiUrl, { mobile, packet_code: packetCode }, {
       headers: { 
@@ -20,6 +25,7 @@ const sendRedeemRequest = async (mobile, packetCode) => {
       },
       timeout: 30000, // Set a timeout to ensure requests don't hang indefinitely
     });
+    // console.log(response.data)
     return response.data;
   } catch (error) {
     console.error(`Error sending POST request: ${error.message}`);
@@ -45,36 +51,39 @@ const extractRedeemCode = (text) => {
 };
 
 (async () => {
-  console.log("Starting bot...");
-  const client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-    useWSS: false,
-    autoReconnect: true,
-    floodSleepThreshold: 120,
-    baseDC: 2,
-    requestTimeout: 43200000,
-  });
+  await client.connect();
+  
+  let lastMessageId = null; // Initialize to track the latest message ID
 
-  await client.start();
-  console.log(`Listening for messages in channels: ${testingChatId}, ${bonusChatId}`);
+  while (true) {
+      try {
+          // Fetch the latest message from the channel
+          const messages = await client.getMessages("@colorwiz_bonus", { limit: 1 });
 
-  client.addEventHandler(async (event) => {
-    const message = event.message;
-    const channelId = message.peerId.channelId.toString();
+          if (messages.length > 0) {
+              const latestMessage = messages[0];
 
-    if (message && (channelId === testingChatId || channelId === bonusChatId)) {
-      const redeemCode = extractRedeemCode(message.message);
+              if (lastMessageId === null || latestMessage.id > lastMessageId) {
+                  lastMessageId = latestMessage.id;
 
-      if (redeemCode) {
-        try {
-          const data = await sendRedeemRequest("+917015957516", redeemCode);
-          await handleRedeemResponse(client, data, "Ankush");
-        } catch (error) {
-          console.error(`Error handling redeem response: ${error.message}`);
-        }
+                  const redeemCode = extractRedeemCode(latestMessage.message);
+
+                  if (redeemCode) {
+                      // console.log(redeemCode)
+                      try {
+                      const data = await sendRedeemRequest("+917015957516", redeemCode);
+                      await handleRedeemResponse(client, data, "Ankush");
+                      } catch (error) {
+                      console.error(`Error handling redeem response: ${error.message}`);
+                      }
+                  }
+              }
+          }
+          await delay(900); 
+      } catch (err) {
+          console.error("Error fetching messages: ", err);
+          // Handle errors or rate limit here
+          await delay(5000); // Backoff strategy
       }
-    }
-  }, new NewMessage({}));
-
-  console.log("Bot running...");
+  }
 })();
